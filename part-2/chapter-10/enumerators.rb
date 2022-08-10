0306923e2a3aac6a67a24d916bf257268a2f98aa
Gustavo Carvalho <gustavo.hcarvalho@outlook.com>
2022-08-10 17:42:19 -0300

    feat: finish subchapter 10.10: Enumerator Semantics and uses

--- part-2/chapter-10/enumerators.rb
+++ part-2/chapter-10/enumerators.rb
@@ -75,3 +75,102 @@ p e.each { |name| name.include?("a") }
 p e.map { |name| name.include?("a") }
 # => [true, true, false, true]
 
+# How to use an enumerator's each method.
+
+array = %w(cat dog rabbit) # look how we don't need to use only []. :)
+
+e = array.map # this will return an enumerator because lots of methods from Enumerable 
+              # returns enumerators when called
+              # without blocks
+
+e.each { |animal| animal.capitalize }
+# => ["Cat", "Dog", "Rabbit"]
+
+# This is not what we normally expect from a "each" return.
+# Here the enumerator uses the map method that we send to our Array object.
+
+=begin
+  def give_me_an_array(array)
+
+  If you pass an array object to this method, the method can alter that object:
+
+    array << "new element"
+
+  If you want to protect the original array from change, you can duplicate it and pass 
+  along the duplicate—or you can pass along an enumerator instead:
+
+    give_me_an_array(array.to_enum)
+
+  The above was extracted directly from the book. It is an awesome example.
+  If we want to pass an array to a method but we don't want that array to change if the
+  method does any operation, we can duplicate that array or, even better, pass an Enumerator.
+
+  The `array.to_enum` will return an Enumerator object with all elements of the array. Also,
+  it will uses the "each" method defined in array as default since we didn't pass any argument to it.
+
+  It will serve as sort o a gateway not allowing any changes because it doesn't absorb any changes.
+=end
+
+class PlayingCard
+  SUITS = %w{ clubs diamonds hearts spades }
+  RANKS = %{ 2 3 4 5 6 7 8 9 10 J Q K A }
+  class Deck
+    def cards
+      @cards.to_enum
+    end
+
+    def initialize(n=1)
+      @cards = []
+      SUITS.cycle(n) do |s|
+        RANKS.cycle(1) do |r|
+          @cards << "#{r} of #{s}"
+        end
+      end
+    end
+  end
+end
+
+# The way we are doing above will adds a layer of protection so it would be hard to change
+# what is inside cards:
+
+deck = PlayingCard::Deck.new
+deck.cards << "Joker!!"
+# => NoMethodError (undefined method '<<' for #<Enumerator:.....> )
+
+
+=begin
+  Enumerators maintain state, so they keep track of where they are in their enumeration.
+  So maybe, because they maintain they initial state, are they frequently used in
+  FP techniques in Ruby? We will learn more later on about this.
+
+  We can also add enumerability to non-enumerable objects:
+=end
+
+module Music
+  class Scale
+    NOTES = %w(c c# d d# e f f# g a a# b)
+    def play
+      NOTES.each { |note| yield note }
+    end
+  end
+end
+
+scale = Music::Scale.new
+scale.play { |note| puts "Next note is #{note}" }
+
+# It will work but we noticed that Scale isn't technically an enumerable:
+
+scale.map { |note| note.upcase } # this will raise an error.
+
+# But if we do:
+
+enum = scale.enum_for(:play) # we are creating an enumerable for the class setting
+                             # the "each" method to behave like "play".
+
+# Now we can do:
+
+p enum.map { |note| note.upcase }
+p enum.select { |note| note.include?("f") }
+
+# Both operations will work because "enum" is an Enumerable Object that we wired up
+# the "play" method of our Scale object.
